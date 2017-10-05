456a65fe433ff3751bd4cd212cedee1179d2acb4
Stephen Burke <sburke4747@gmail.com>
2017-10-05 15:39:50 +0000

    Added ul's to first block, fixed external routing

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
 
+//Internal Routing
+
 app.get("/", function(req, res){
     res.render("landing");
 });
@@ -29,6 +31,29 @@ app.get("/pricing", function(req, res){
 app.get("/services", function(req, res){
     res.render("services");
 });
+
+//External Routing
+
+app.get("/MySite", function(req, res){
+    res.redirect("https://www.google.com/");
+});
+
+app.get("/Instagram", function(req, res){
+    res.redirect("https://www.instagram.com/");
+});
+
+app.get("/Facebook", function(req, res){
+    res.redirect("https://www.facebook.com/");
+});
+
+app.get("/Twitter", function(req, res){
+    res.redirect("https://www.twitter.com/");
+});
+
+app.get("/Pintrest", function(req, res){
+    res.redirect("https://www.pintrest.com/");
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("EventsByRonnie server has started!");
 })
